a4e24e6f32a7f4794274beb06b703684a0ea5e96
Muhammad Wajahat Raheel <134369574+wajahatraheel@users.noreply.github.com>
2024-03-20 14:55:17 +0100

    Recipe Test done. Ready to Integrate into python generics

--- EOJ Python/ZL_Read_Recipe.py
+++ EOJ Python/ZL_Read_Recipe.py
@@ -6,14 +6,32 @@ import os
 
 
 
-
 class ZL_Read_Recipe():
+    '''
+    Developed by M.WAJAHAT RAHEEL 30/03/2024.
+    Reads Recipe table
+
+    The Reading parameters first needs to be defined. These parameters are placed in SQlite3 database with table as named
+    and the fields are groups or Gruppe in recipe table.
+
+    INPUTS
+    dmc : str
+    receipe_file_directory : str
+    sqlite_path : str
+    sqlite_table_name : str
+
+
+    OUTPUTS :
+    recipe_output = dictionary
+    recipe_read = BOOL
+
+    '''
 
 
     def __init__(self, logger = None):
 
-        self.recipe_output: dict = {}
-        self.recipe_read: bool = True
+        self.recipe_output: dict = {}  # OUTPUT
+        self.recipe_read: bool = True  # IF READ SUCCESSFUL
         self.__error = False
 
         if logger == None:
@@ -23,9 +41,34 @@ class ZL_Read_Recipe():
 
 
     def check_article_no(self,dmc):
+
+        '''
+        the function first checks the Data Matrix Code of laser
+        then it checks if the article has 9 numbers. it will not yet check if the article exist in Recipe Table
+        it will come later.
+        :param dmc:
+        :return: article_no , self.__error
+        '''
+
+        month_dict = {"A" : "1", "B" : "2","C" : "3", "D" : "4","E" : "5", "F" : "6","G" : "7", "H" : "8",
+                      "I" : "9", "J" : "10","K" : "11", "L" : "12"}
+
         try:
             separated_characters = dmc.split(';')
             article_no = separated_characters[1]
+            serial_no = separated_characters[0]
+            serial_no_year = serial_no[0:2]
+            month_no = separated_characters[len(separated_characters)-1]
+            month_no = month_no[0]
+            serial_with_extension = serial_no+'.tdms'
+            tdms_path = (os.path.join('U:\\TDMS_Datenbank\Daten', serial_no_year, month_dict[month_no],serial_with_extension))
+
+            if os.path.exists(tdms_path):
+                self.__error = False
+            else:
+                self.__error = True
+                logging.error('Error : DMC Read : TDMS file not reachable :')
+
         except:
             self.__error = True
             self.recipe_read = False
@@ -49,6 +92,7 @@ class ZL_Read_Recipe():
 
         del separated_characters
 
+
         return article_no
 
     def __check_if_outdic_emptyvalues(self,output_dic : dict):
@@ -62,18 +106,14 @@ class ZL_Read_Recipe():
                 item_empty = str(key_lst[i]) + '  :Empty Values in Recipe Table.'
                 logging.error(item_empty)
 
-
-
-
-
     def __read_sqlite_parm_names(self, table_name,sqlite_path):
 
         if self.__error == False:
             sq_lite_database = sqlite3.connect(sqlite_path)
             try :
 
-                sql_query = """SELECT name FROM sqlite_master 
-                        WHERE type='table';"""
+                #sql_query = """SELECT name FROM sqlite_master
+                        #WHERE type='table';"""
                 '''cursor: object = sq_lite_database.cursor()
                 # executing our sql query
                 cursor.execute(sql_query)
@@ -88,7 +128,6 @@ class ZL_Read_Recipe():
             except Exception as e:
                 db_dataframe = []
                 self.recipe_read = False
-                str_error = str (e) + ' in SQlite3 database'
                 logging.error('No table in Sqlite3 Database : read_recipe_table')
         if self.__error == True:
             db_dataframe = []
@@ -99,8 +138,8 @@ class ZL_Read_Recipe():
     def read_recipe_table(self, module_dmc: str, recipe_file_address: str,sqlite_path : str , sqlite_table_name : str):
 
         recipe_group_row = 'Gruppe'  # See Recezipt.csv file for row named as 'Gruppe'
-        recipe_parameter_row = 'Parameter'
-        recipe_datentype_row = 'DatenTyp'
+        recipe_parameter_row = 'Parameter'# See Recezipt.csv file for row named as 'PARAMETER'
+        recipe_datentype_row = 'DatenTyp'# See Recezipt.csv file for row named as 'Datentyp'
         required_group_index_array = [0]
         temp_index = 0
         database_dataframe = []
@@ -126,8 +165,6 @@ class ZL_Read_Recipe():
             self.recipe_read = False
         else:
             self.__error = False
-
-
 
         if self.__error == False:
 
